c8df43b27306008615308c6f676fbe39afc7d8f2
Ayush <tripathyayush007@gmail.com>
2025-09-16 17:02:48 +0530

    Added Route to fetch a Single image and also to a list of  get image urls from the server.

--- ai_service/app/main.py
+++ ai_service/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routes import document_routes, json_routes
+from app.routes import document_routes, image_routes, json_routes
 
 app = FastAPI(title="AI Service Backend")
 
 # Register routes
 app.include_router(document_routes.router)
 app.include_router(json_routes.router)
+app.include_router(image_routes.router)
